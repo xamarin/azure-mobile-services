88853f9ffdbb9b842899273e7212b5e50c04b6ca
ermau <me@ermau.com>
2013-05-01 20:27:32 -0400

    Correct AssemblyInfo

--- sdk/xamarin/ios/src/Microsoft.Azure.Zumo.iOS/Properties/AssemblyInfo.cs
+++ sdk/xamarin/ios/src/Microsoft.Azure.Zumo.iOS/Properties/AssemblyInfo.cs
@@ -1,4 +1,13 @@
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
+[assembly: AssemblyTitle("Microsoft.WindowsAzure.MobileServices.iOS")]
+[assembly: AssemblyDescription("Mobile Services Client Library for Xamarin.iOS")]
+[assembly: AssemblyProduct("Microsoft.WindowsAzure.MobileServices")]
+[assembly: AssemblyCopyright("Copyright © Microsoft Corporation; Copyright 2013 Xamarin Inc")]
+[assembly: ComVisible(false)]
 
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
 [assembly: InternalsVisibleTo ("ZumoTests")]
